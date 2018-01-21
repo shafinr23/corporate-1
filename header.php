8643f802d3171f166bf983afa74c255065212fae
ShafinRahman <33410193+shafinr23@users.noreply.github.com>
2018-01-21 23:28:16 +0600

    add wp head wp foot

--- header.php
+++ header.php
@@ -9,18 +9,7 @@
 
     <title>Corporate 1</title>
 
-    <!-- Bootstrap Core CSS -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-
-    <!-- Custom CSS: You can use this stylesheet to override any Bootstrap styles and/or apply your own styles -->
-    <link href="css/custom.css" rel="stylesheet">
-
-    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-        <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
-        <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
-    <![endif]-->
+<?php wp_head(); ?>
 
 </head>
 
